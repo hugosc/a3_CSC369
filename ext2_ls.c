af0801f345530faa090296ee94af39a8a3cccca6
Hugo Sales Corrêa <hugosalescorrea@gmail.com>
2015-12-03 02:56:03 -0500

    bla2

--- ext2_ls.c
+++ ext2_ls.c
@@ -1,4 +1,5 @@
 #include "ext2_utils.h"
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
--- ext2_utils.h
+++ ext2_utils.h
@@ -1,19 +1,14 @@
 #ifndef EXT2_UTILS_H
 #define EXT2_UTILS_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
+
 #include "ext2.h"
+#include <stdlib.h>
 
 extern unsigned char * disk;
 extern struct ext2_super_block super_block;
 
 int ext2_init(const char * disk_image);
-struct ext2_inode* find_node(const char * filepath);
+struct ext2_inode* find_inode(const char * filepath);
 
 #endif //EXT2_UTILS_H
